a3020aeb0530a028611c5a34feb72a0e345f6c01
okipre-dotcom <t.keru.oki.pre@gmail.com>
2026-06-13 19:19:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-// FP1級トレーナー オフラインキャッシュ
-const C='fp1-trainer-v4.15';
-const ASSETS=['./','./index.html','./FP1tra_v4.15_PWA.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const C='fp1-trainer-v5.00';
+const ASSETS=['./','./index.html','./FP1tra_v5.00_PWA.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));return res})))});
