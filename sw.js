39397967b3ee37b2959fd7d87b22e4b9d20ecf6e
okipre-dotcom <t.keru.oki.pre@gmail.com>
2026-06-15 19:06:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='fp1-trainer-v5.6';
-const ASSETS=['./','./index.html','./FP1tra_v5.6_PWA.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const C='fp1-trainer-v5.9';
+const ASSETS=['./','./index.html','./FP1tra_v5.9_PWA.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(C).then(c=>c.put(e.request,cp));return res})))});
